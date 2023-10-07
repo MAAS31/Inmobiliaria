dbf0c66309095bd0c7cfed3e68facba365cf164c
hardmand <maury.alvarez@hotmail.com>
2023-10-07 01:54:02 -0500

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,25 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import pickle 
+import streamlit as st 
+import numpy as np
+from catboost import CatBoostRegressor
+import sklearn
 
-"""
-# Welcome to Streamlit!
+st.title("Modelo Boosting")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.divider()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.write("Ingrese los datos")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+assess= st.slider("Assess",0,20)
+bdrms= st.slider("Bdrms",0,50)
+lotsize= st.slider("Lotsize",0,20)
+sqrft= st.slider("sqrft",0, 10)
+colonial=st.slider("colonial",0,1)
+
+with open ("model.pkl",'rb') as doc:
+    model= pickle.load(doc)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+prediccion = model.predict(np.array([[assess, bdrms, lotsize, sqrft, colonial]]))
+if st.button("Predecir:"):
+    st.write(f"El precio es de {prediccion[0]}")
